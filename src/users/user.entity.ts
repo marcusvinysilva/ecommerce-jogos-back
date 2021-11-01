21a22c6f7828620b84008b2ba97c807e1182148d
Marcus <mvsilva1997@gmail.com>
2021-11-01 20:00:00 -0300

    create user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,64 @@
+import {
+  BaseEntity,
+  Entity,
+  Unique,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Entity()
+@Unique(['email', 'cpf'])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  name: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  lastName: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  cpf: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  phone: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  address: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  role: string;
+
+  @Column({ nullable: false, default: true })
+  status: boolean;
+
+  @Column({ nullable: false, type: 'varchar' })
+  email: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  password: string;
+
+  @Column({ nullable: false })
+  salt: string;
+
+  @Column({ nullable: true, type: 'varchar' })
+  confirmationToken: string;
+
+  @Column({ nullable: true, type: 'varchar' })
+  recoverToken: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+
+  async checkPassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
+}
