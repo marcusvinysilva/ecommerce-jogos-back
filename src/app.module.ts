25ebc7fc046d7a3c839058c9ffdc4677da42c170
Marcus <mvsilva1997@gmail.com>
2021-11-03 15:39:59 -0300

    add typeORM module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './configs/typeorm.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UsersModule],
   controllers: [],
   providers: [],
 })
