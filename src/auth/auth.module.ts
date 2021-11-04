257e36607f2538d32444304d8572b3370d8567e0
Marcus <mvsilva1997@gmail.com>
2021-11-04 20:16:50 -0300

    added jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from 'src/users/users.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'secretKey',
+      signOptions: {
+        expiresIn: '30d',
+      },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
 })
