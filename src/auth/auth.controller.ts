a1de68f387f84eb5def2a617af814bf4f36dcf26
Marcus <mvsilva1997@gmail.com>
2021-11-05 17:26:45 -0300

    added route change password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,11 +5,13 @@ import {
   Param,
   Patch,
   Post,
+  UnauthorizedException,
   UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { UserRole } from 'src/users/user-roles.enum';
 import { User } from 'src/users/user.entity';
 import { AuthService } from './auth.service';
 import { ChangePasswordDto } from './change-password.dto';
@@ -67,7 +69,26 @@ export class AuthController {
   ): Promise<{ message: string }> {
     await this.authService.resetPassword(token, changePasswordDto);
     return {
-      message: 'Senha alterada com sucesso',
+      message: 'Password changed successfully',
+    };
+  }
+
+  @Patch(':id/change-password')
+  @UseGuards(AuthGuard())
+  async changePassword(
+    @Param('id') id: string,
+    @Body(ValidationPipe) changePasswordDto: ChangePasswordDto,
+    @GetUser() user: User,
+  ) {
+    if (user.role != UserRole.ADMIN && user.id.toString() != id) {
+      throw new UnauthorizedException(
+        'You do not have permission to perform this operation.',
+      );
+    }
+
+    await this.authService.changePassword(id, changePasswordDto);
+    return {
+      message: 'Password changed successfully',
     };
   }
 }
