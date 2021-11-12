6a4177ecf3578f9f9171b93f2898fdc2608fdf0d
Marcus <mvsilva1997@gmail.com>
2021-11-11 21:16:59 -0300

    connect db heroku

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,11 +2,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const TypeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'kesavan.db.elephantsql.com',
+  host: 'ec2-35-169-204-98.compute-1.amazonaws.com',
   port: 5432,
-  username: 'wpyqplkp',
-  password: 'OqHgPvIDQKq43rVmQKA91jhu6qnLaCZN',
-  database: 'wpyqplkp',
+  username: 'hrlopqpjztndbb',
+  password: '5bb9466bbf72e592500cd4f3941894a60f1fc625dcf994484ea4f28bc474454c',
+  database: 'd33gqbdv0jrj79',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
