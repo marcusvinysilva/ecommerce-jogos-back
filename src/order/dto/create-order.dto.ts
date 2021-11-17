ce03b59579791568c7aab288b040e7da9efc0bc1
Marcus <mvsilva1997@gmail.com>
2021-11-17 19:38:25 -0300

    add create order swagger

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -2,11 +2,14 @@
 import { IsNotEmpty } from 'class-validator';
 import { User } from 'src/users/user.entity';
 import { Game } from 'src/games/game.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOrderDto {
   @IsNotEmpty()
+  @ApiProperty()
   user: User;
 
   @IsNotEmpty()
+  @ApiProperty()
   games: Game;
 }
