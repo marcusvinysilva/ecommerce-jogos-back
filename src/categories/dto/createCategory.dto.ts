867dbc2495ec2294ace513776038ddc674f7412c
Marcus <mvsilva1997@gmail.com>
2021-11-17 19:39:05 -0300

    add categories dtos swagger

--- src/categories/dto/createCategory.dto.ts
+++ src/categories/dto/createCategory.dto.ts
@@ -1,8 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 }
 
